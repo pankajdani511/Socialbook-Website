7e2d0bda1601de45e1caa017c1e5a89424221bbb
pankajdani511 <pankajdani511@gmail.com>
2026-02-07 20:08:49 +0530

    add dark mood logic

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
- let settingsmenu=document.querySelector(".settings-menu");
-
+let settingsmenu=document.querySelector(".settings-menu");
+let darkBtn=document.getElementById("dark-btn");
 
 
 function settingsMenuToggle(){
     settingsmenu.classList.toggle("settings-menu-height");
 }
+
+darkBtn.onclick=function(){
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+    if(localStorage.getItem("theme")=="light"){
+        localStorage.setItem("theme","dark");
+    }else{
+        localStorage.setItem("theme","light");
+    }
+
+}
+
 
