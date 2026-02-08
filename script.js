98eeab9ddb1ed68340d242827ac0aa7fb41add10
pankajdani511 <pankajdani511@gmail.com>
2026-02-08 11:53:30 +0530

    complete js logic

--- script.js
+++ script.js
@@ -19,3 +19,14 @@ darkBtn.onclick=function(){
 }
 
 
+if(localStorage.getItem("theme")=="light"){
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+
+}else if(localStorage.getItem("theme")=="dark"){
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+}
+else{
+    localStorage.setItem("theme","light");
+}
